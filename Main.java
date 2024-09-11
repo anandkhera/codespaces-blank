bb3409ccea8b09a79af7fa75b9d874b9adf88cb9
anandkhera <anandk3@nycstudents.net>
2024-09-11 15:48:14 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+    public static void main(String[] args) {
+        String l = "l";
+        int s = 5;
+        double r = 2.2;
+        boolean a = true;
+        r = r +s;
+        System.out.println(l + s + r + a);    
+    }
+    
+}
